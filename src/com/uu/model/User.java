7d4b2be25e22d127ca20b7242659a6f2ccae0114
liupenghao <anhusky@163.com>
2018-09-18 18:50:05 +0800

    add User

---
+++ src/com/uu/model/User.java
@@ -0,0 +1,37 @@
+package com.uu.model;
+
+/**
+ * 描述：
+ *
+ * @author liupenghao
+ * @create 2018-09-18 下午6:47
+ **/
+public class User {
+    private String name;
+    private String description;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
