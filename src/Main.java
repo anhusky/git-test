6da260471596b05cd2d43716643764a4b3a10131
liupenghao <anhusky@163.com>
2018-09-18 19:09:41 +0800

    User idea add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import com.uu.model.User;
+import org.junit.Test;
+
 public class Main {
     // 合并一下喽 😁😝
-    public static void mai1n(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!  12345777");
 
+    }
+
+    @Test
+    public void println(User user) {
+        System.out.println(user.toString());
     }
 }
